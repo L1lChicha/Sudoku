935177c43c24f47a6e372e32fe23f7a3f707904a
L1lChicha <kalidailya1205@gmail.com>
2024-01-22 14:37:02 +0100

    Revert "Added difficulty and changes in Gui"
    This reverts commit 37d3629ca96c0c5bd665e48b2a2ba08fcb0f3437.

--- sudoku/PlayGround.xaml.cs
+++ sudoku/PlayGround.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,9 +24,6 @@ namespace sudoku
             InitializeComponent();
         }
 
-        private void pauseButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+       
     }
 }
