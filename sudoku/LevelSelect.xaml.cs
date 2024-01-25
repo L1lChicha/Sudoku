e675b8db7c007acc85ab478f6afc468cfcd6c62a
L1lChicha <kalidailya1205@gmail.com>
2024-01-25 20:30:31 +0100

    GUI level difficulty, added validation

---
+++ sudoku/LevelSelect.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sudoku
+{
+    /// <summary>
+    /// Логика взаимодействия для LevelSelect.xaml
+    /// </summary>
+    public partial class LevelSelect : Window
+    {
+        PlayGround playGround = new PlayGround();
+
+        public LevelSelect()
+        {
+            InitializeComponent();
+
+
+          
+        }
+
+        private void easyRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            playGround.Show();
+            Close();
+        }
+
+        private void middleRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            playGround.Show();
+            Close();
+        }
+
+        private void hardRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            playGround.Show();
+            Close();
+        }
+    }
+}
